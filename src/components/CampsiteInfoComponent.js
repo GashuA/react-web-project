5b72038a0020b173fe70ee06d28a16997c59b7d5
Gashaye <gashayem@gmail.com>
2022-03-09 19:02:27 -0500

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,53 +1,47 @@
-import { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component{
+function RenderCampsite({campsite}){
+  return( 
+    <div className="col-md-5 and m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
 
-  renderCampsite(campsite){
-    return( 
-      <div className="col-md-5 and m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+function RenderComments({comments}){
+  if(comments){
+    return(
+      <div className="col-md-5 m-1">
+        <h4>comments</h4>
+        { comments.map(comment =>
+          <span key ={comment.id}> {comment.text} <br/> 
+          -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}<br/>
+          </span> )
+        }
       </div>
     )
   }
-
-  renderComments(comments){
-    if(comments){
-      return(
-        <div className="col-md-5 m-1">
-          <h4>comments</h4>
-          { comments.map(comment =>
-            <span key ={comment.id}> {comment.text} <br/> 
-            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}<br/>
-            </span> )
-          }
-        </div>
-      )
-    }
-    return <div/>
-  }
+  return <div/>
+}
     
-  render(){
-    const {campsite} = this.props; //this.prop replaced with newly defined Variable
-    if(campsite){
-      return(
-        <div className="container">
-          <div className="row"> 
-            {this.renderCampsite(campsite)} 
-            {this.renderComments(campsite.comments)}  
-          </div>
+function CampsiteInfo(props){
+  if(props.campsite){
+    return(
+      <div className="container">
+        <div className="row"> 
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} />  
         </div>
-
-      );
-    }
-    return <div/>;
+      </div>
+    );
   }
+  return <div/>;
 }
 
 export default CampsiteInfo;
