3ff1ee55f52d364a2484aae740f275cf845072bd
Gashaye <gashayem@gmail.com>
2022-03-24 18:48:21 -0400

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import { Link } from 'react-router-dom';
 import { Component } from 'react';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 const maxNameLength = nameLen => nameVal => !(nameVal) || (nameVal.length <= nameLen);
@@ -40,6 +41,26 @@ function RenderComments({comments, addComment, campsiteId}){
 }
     
 function CampsiteInfo(props){
+  if (props.isLoading) {
+    return (
+        <div className="container">
+            <div className="row">
+                <Loading />
+            </div>
+        </div>
+    );
+  }
+  if (props.errMess) {
+    return (
+      <div className="container">
+          <div className="row">
+              <div className="col">
+                  <h4>{props.errMess}</h4>
+              </div>
+          </div>
+      </div>
+    );
+    }
   if(props.campsite){
     return (
       <div className="container">
